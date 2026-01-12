9258c63f49cfefe0fa47cdddefcd0e2830973f65
daniel3g <silvadaniel2006@yahoo.com.br>
2026-01-12 11:30:29 -0300

    ajustes import

--- src/app/ebooks/como-escrever-uma-redacao-de-sucesso/page.tsx
+++ src/app/ebooks/como-escrever-uma-redacao-de-sucesso/page.tsx
@@ -1,25 +1,24 @@
-import PDFViewer from '@/components/PDFViewer'
-import HeaderDashboard from '@/components/SidebarDashboard'
-import { createClient } from '../../../../utils/supabase/server'
+import PDFViewer from "@/components/PDFViewer";
+import SidebarDashboard from "@/components/SidebarDashboard";
+import { createClient } from "../../../../utils/supabase/server";
 
 export default async function EbookPage() {
-  const supabase = await createClient()
+  const supabase = await createClient();
 
   const { data } = await supabase.storage
-    .from('ebooks')
-    .createSignedUrl('como-escrever-uma-redacao-de-sucesso.pdf', 60 * 5) // 5 min
+    .from("ebooks")
+    .createSignedUrl("ciencias-humanas.pdf", 60 * 5);
 
-  if (!data?.signedUrl) {
-    return <p>Erro ao carregar o PDF</p>
-  }
+  if (!data?.signedUrl) return <p>Erro ao carregar o PDF</p>;
 
   return (
-    <div>
-      <HeaderDashboard />
+    <SidebarDashboard>
       <main className="flex flex-col w-4/5 m-auto py-10">
         <h1 className="text-2xl font-bold mb-4">Visualizando: Como escrever uma redação de sucesso</h1>
         <PDFViewer url={data.signedUrl} />
       </main>
-    </div>
-  )
+    </SidebarDashboard>
+  );
 }
+
+
